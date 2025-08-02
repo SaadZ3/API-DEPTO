ee7f9b8cf5d2cafd237deeab788372bf7dc5132d
SaadZ3 <saadmu139@gmail.com>
2025-08-02 17:07:22 -0300

    atualizando BD com role 05

--- dist/controllers/HomeControllers.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
-  async index(req, res) {
-    res.json('index');
-  }
-}
-
-// exportando a classe ja instanciada
-exports. default = new HomeController();
