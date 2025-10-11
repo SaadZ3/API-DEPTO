594465b1e1b9dfc50dd94f74caf860c14cb3a50b
SaadZ3 <saadmu139@gmail.com>
2025-10-11 17:25:32 -0300

    atualizando campos de andar3

--- src/database/migrations/20251011200837-alterar-andar-departamentos.js
+++ src/database/migrations/20251011200837-alterar-andar-departamentos.js
@@ -1,10 +1,9 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    // Remove a coluna 'andar' antiga
+  up: async (queryInterface, Sequelize) => {
+    // SEU CÓDIGO DA FUNÇÃO 'up' VEM AQUI
+    // ...
     await queryInterface.removeColumn('departamentos', 'andar');
-
-    // Adiciona a nova coluna 'andar' com as opções predefinidas
     await queryInterface.addColumn('departamentos', 'andar', {
       type: Sequelize.ENUM(
         '1º Sub-solo',
@@ -21,25 +20,24 @@ module.exports = {
         '10º Andar',
       ),
       allowNull: false,
-      defaultValue: 'Térreo', // Define um valor padrão
+      defaultValue: 'Térreo',
     });
-
-    // Adiciona a coluna 'complemento'
     await queryInterface.addColumn('departamentos', 'complemento', {
       type: Sequelize.STRING,
-      allowNull: true, // Permite que seja nulo
+      allowNull: true,
     });
+    // ...
   },
 
-  async down(queryInterface, Sequelize) {
-    // Reverte as alterações
+  down: async (queryInterface, Sequelize) => {
+    // SEU CÓDIGO DA FUNÇÃO 'down' VEM AQUI
+    // ...
     await queryInterface.removeColumn('departamentos', 'andar');
     await queryInterface.removeColumn('departamentos', 'complemento');
-
-    // Adiciona a coluna 'andar' antiga de volta
     await queryInterface.addColumn('departamentos', 'andar', {
       type: Sequelize.STRING,
       allowNull: false,
     });
+    // ...
   },
 };
