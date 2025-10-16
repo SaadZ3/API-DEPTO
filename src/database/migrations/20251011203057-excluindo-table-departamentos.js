3cd8e2bb3c48e451ed4bb7fc35b90a8bcab26499
Saad Amer <167485815+SaadZ3@users.noreply.github.com>
2025-10-16 12:56:56 -0300

    Delete src/database/migrations/20251011203057-excluindo-table-departamentos.js

--- src/database/migrations/20251011203057-excluindo-table-departamentos.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface) {
-    await queryInterface.dropTable('departamentos');
-  },
-
-  // eslint-disable-next-line no-empty-function
-  async down() {},
-};
