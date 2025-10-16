ca698d16783df9a416d89a53c0b9825c5296eccc
Saad Amer <167485815+SaadZ3@users.noreply.github.com>
2025-10-16 12:56:49 -0300

    Delete src/database/migrations/20250920194615-drop-fotos.js

--- src/database/migrations/20250920194615-drop-fotos.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface) {
-    await queryInterface.dropTable('alunos');
-  },
-
-  // eslint-disable-next-line no-empty-function
-  async down() {},
-};
